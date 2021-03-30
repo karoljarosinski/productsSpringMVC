b66e7a92b70249de351c35b2f4776e604dd60a6e
karol <karol.jarosinski91@gmail.com>
2021-03-30 07:12:39 +0200

    empty

---
+++ src/main/java/com/kj/productsSpringMVC/ProductsSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.kj.productsSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductsSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kj/productsSpringMVC/ProductsSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kj.productsSpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
